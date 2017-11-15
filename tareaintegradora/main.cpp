706a6d4d3b53fa58e2a5d620f149821b18c442db
Jose Antonio <joseantonio@MacBook-Pro-de-Jose.local>
2017-11-14 21:33:35 -0600

    Initial Commit

---
+++ tareaintegradora/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  tareaintegradora
+//
+//  Created by Jose Antonio on 14/11/17.
+//  Copyright © 2017 Jose Antonio. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
